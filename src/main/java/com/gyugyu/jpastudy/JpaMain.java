693a43ddb6c36797933fe31409d94dfb666541af
gyugyu90 <kh2000park@gmail.com>
2019-12-23 14:11:22 +0900

    5장 예제

--- src/main/java/com/gyugyu/jpastudy/JpaMain.java
+++ src/main/java/com/gyugyu/jpastudy/JpaMain.java
@@ -2,6 +2,7 @@ package com.gyugyu.jpastudy;
 
 import com.gyugyu.jpastudy.model.Board;
 import com.gyugyu.jpastudy.model.Member;
+import com.gyugyu.jpastudy.model.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -27,8 +28,13 @@ public class JpaMain {
 
             tx.begin(); //트랜잭션 시작
 //            logic(em);  //비즈니스 로직
-            logicBoard(em);
+            testSave(em);
+//            queryLogicJoin(em);
+//            updateRelation(em);
+//            deleteRelation(em);
+            biDirection(em);
             tx.commit();//트랜잭션 커밋
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -72,4 +78,66 @@ public class JpaMain {
         em.persist(board);
         System.out.println("board.id = " + board.getId());
     }
+
+    private static void testSave(EntityManager em) {
+        // 팀 1 저장
+        Team team1 = new Team();
+        team1.setId("team1");
+        team1.setName("팀1");
+        em.persist(team1);
+
+        // 회원 1 저장
+        Member member1 = new Member();
+        member1.setId("member1");
+        member1.setUsername("회원1");
+        member1.setAge(20);
+        member1.setTeam(team1);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setId("member2");
+        member2.setUsername("회원2");
+        member2.setAge(20);
+        member2.setTeam(team1);
+        em.persist(member2);
+    }
+
+    private static void queryLogicJoin(EntityManager em) {
+        String jpql = "select m from Member m join m.team t where t.name=:teamName";
+
+        List<Member> resultList = em.createQuery(jpql, Member.class)
+                .setParameter("teamName", "팀1")
+                .getResultList();
+
+        for (Member member: resultList) {
+            System.out.println("[query] member.username = " + member.getUsername());
+        }
+    }
+
+    private static void updateRelation(EntityManager em) {
+        // 새로운 팀
+        Team team2 = new Team();
+        team2.setId("team2");
+        team2.setName("팀2");
+        em.persist(team2);
+
+        // 회원1에 새로운 팀2 설정
+        Member member = em.find(Member.class, "member1");
+        member.setTeam(team2);
+    }
+
+    private static void deleteRelation(EntityManager em) {
+        Member member1 = em.find(Member.class, "member1");
+        member1.setTeam(null); // 연관관계 제거
+    }
+
+    private static void biDirection(EntityManager em) {
+        Team team = em.find(Team.class, "team1");
+        List<Member> members = team.getMembers(); // (팀 -> 회원) 객체 그래프 탐색
+
+        for(Member member: members) {
+            System.out.println("member.username = " + member.getUsername());
+        }
+    }
+
 }
--- src/main/java/com/gyugyu/jpastudy/model/Member.java
+++ src/main/java/com/gyugyu/jpastudy/model/Member.java
@@ -1,11 +1,16 @@
 package com.gyugyu.jpastudy.model;
 
+import lombok.Data;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -21,6 +26,7 @@ import java.util.Date;
         name = "NAME_AGE_UNIQUE",
         columnNames = {"NAME", "AGE"}
 )})
+@Data
 public class Member {
 
     @Id
@@ -44,59 +50,9 @@ public class Member {
     @Lob
     private String description;
 
-    public String getId() {
-        return id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
